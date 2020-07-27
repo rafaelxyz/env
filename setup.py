13b31e0e589add7a6faaee3cb51acf212d25103f
Rafael Odzakow <rafael.odzakow@ericsson.com>
2020-07-27 23:48:46 +0200

    sf

--- setup.py
+++ setup.py
@@ -70,6 +70,10 @@ def setup_environment():
     os.system('git clone https://github.com/ranger/ranger.git')
     os.system('git clone https://github.com/alexanderjeurissen/ranger_devicons.git')
     os.system('xrdb ~/.Xresources')
+    os.system('wget https://github.com/neovim/neovim/releases/download/v0.4.3/nvim.appimage')
+    os.system('chmod u+x nvim.appimage')
+    os.system('cp nvim.appimage /usr/bin/vi')
+    os.system('cp nvim.appimage /usr/bin/vim')
 
 
 setup_environment()
