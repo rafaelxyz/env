998e481b7ea91e20e0c12b142d78ff16f840f422
Rafael Odzakow <rafael.odzakow@trioptima.com>
2024-06-18 16:39:08 +0200

    blame keymap

--- .config/nvim/lua/plugins/blame.lua
+++ .config/nvim/lua/plugins/blame.lua
@@ -1,6 +1,6 @@
 return {
   "FabijanZulj/blame.nvim",
-  keys = { {"<leader>gB", "<cmd>BlameToggle<CR>", desc="BlameToggle"}},
+  keys = { {"<leader>gt", "<cmd>BlameToggle<CR>", desc="BlameToggle"}},
   config = function()
     require("blame").setup()
   end
