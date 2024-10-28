fbd8faefcdefef8df803526bea005bc4361056b7
Rafael Odzakow <rafael.odzakow@trioptima.com>
2024-10-28 23:26:04 +0100

    comments fix

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -17,8 +17,6 @@ end
 opt.relativenumber = false
 opt.clipboard = "unnamed"
 
--- LSP Server to use for Python.
--- Set to "basedpyright" to use basedpyright instead of pyright.
+-- Set to "basedpyright" or "pyright".
 vim.g.lazyvim_python_lsp = "basedpyright"
--- Set to "ruff_lsp" to use the old LSP implementation version.
 vim.g.lazyvim_python_ruff = "ruff"
