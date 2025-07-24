7d5b811f8e1ba000f50cbb265bebb72ec799dcf4
Rafael Odzakow <rafael.odzakow@trioptima.com>
2025-07-24 17:16:10 +0200

    hidden no more

--- .config/nvim/lua/plugins/lazy.lua
+++ .config/nvim/lua/plugins/lazy.lua
@@ -75,4 +75,15 @@ return {
       }
     }
   },
+
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        hidden = true, -- for hidden files
+        ignored = true, -- for .gitignore files
+        sources = { files = { hidden = true } }
+      },
+    },
+  },
 }
